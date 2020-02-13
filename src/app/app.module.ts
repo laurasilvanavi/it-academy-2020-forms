39e42c3c4187ce0a971faf4dc5f242a5a5f742e9
Laura Silvanaviciute <p998ljj@fspa.myntet.se>
2020-02-14 00:24:43 +0200

    refactor: question form updated to reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    // FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/question-form/question-form.component.ts
+++ src/app/question-form/question-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Question } from '../models/question';
 import { QuestionService } from '../services/question.service';
+import { forbiddenNameValidator } from '../shared/forbidden-name-directive';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-question-form',
@@ -14,7 +16,32 @@ export class QuestionFormComponent implements OnInit {
   question: Question;
   serverErrorMessage: string;
 
-  constructor(private questionService: QuestionService) { }
+  // questionForm = new FormGroup({
+  //   senderName: new FormControl(''),
+  //   senderEmail: new FormControl(''),
+  //   category: new FormControl(''),
+  //   details: new FormControl(''),
+  // });
+  questionForm = this.fb.group({
+    senderName: ['', [
+      Validators.required,
+      Validators.maxLength(30),
+      forbiddenNameValidator(/Laura/i)
+    ]],
+    senderEmail: ['', [
+      Validators.required,
+      Validators.maxLength(30),
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+    ]],
+    category: ['', Validators.required],
+    details: ['Details', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(30)
+    ]],
+  });
+
+  constructor(private questionService: QuestionService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.categories = [];
@@ -28,6 +55,7 @@ export class QuestionFormComponent implements OnInit {
   }
 
   onSubmit() {
+    this.question = this.questionForm.value;
     this.questionService.addQuestion(this.question).subscribe(() => {
       this.question = new Question('', '', '', '');
       this.serverErrorMessage = '';
@@ -36,4 +64,8 @@ export class QuestionFormComponent implements OnInit {
     );
   }
 
+  get senderName() { return this.questionForm.get('senderName'); }
+  get senderEmail() { return this.questionForm.get('senderEmail'); }
+  get category() { return this.questionForm.get('category'); }
+  get details() { return this.questionForm.get('details'); }
 }
